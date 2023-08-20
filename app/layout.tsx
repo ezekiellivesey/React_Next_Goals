26963b1ac524fcc11c1fdddca811ca683e1eb9bc
ezekiellivesey <85894082+ezekiellivesey@users.noreply.github.com>
2023-08-20 20:51:04 +1000

    Added online database + unit testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import NavMenu from './NavMenu'
 import AuthProvider from './AuthProvider'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +17,7 @@ export default function RootLayout({
   return (
     <AuthProvider>
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NavMenu />
         {children}
         </body>
---
+++ app/sum.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+// write a typescript function that takes two numbers and returns their sum
+
+export function sum(a: number, b: number): number {
+  return a + b;
+}
